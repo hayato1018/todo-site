c3f6eb6b28f0c08516ce39453a6a796e6778fbdc
hayato_sawamura <hayato.oorer1018@gmail.com>
2024-09-19 21:03:43 +0900

    lombokの適用

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,18 +1,16 @@
 package com.example.todo.controller.task;
 
 import com.example.todo.service.task.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
-
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
 
     @GetMapping("/tasks")
     public String list(Model model) {
