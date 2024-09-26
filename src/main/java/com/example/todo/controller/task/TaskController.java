a92e5b46e555fb07c1c2c8f00f1a272bc6d54021
hayato_sawamura <hayato.oorer1018@gmail.com>
2024-09-26 22:15:38 +0900

    リファクタリング追加

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -4,10 +4,9 @@ import com.example.todo.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,7 +25,7 @@ public class TaskController {
         return "tasks/list";
     }
 
-    @GetMapping("{id}")    // GET /tasks/detail
+    @GetMapping("{id}")
     public String showDetail(@PathVariable("id") long taskId, Model model) {
         model.addAttribute("taskId", taskId);
         var taskEntity = taskService.findById(taskId)
@@ -35,15 +34,16 @@ public class TaskController {
         return "tasks/detail";
     }
 
-    // GET /tasks/creationForm
-    @GetMapping("creationForm")
-    public String showCreationForm() {
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute() TaskForm form) {
         return "tasks/form";
     }
 
-    // POST /tasks
     @PostMapping
-    public String create(TaskForm form) {
+    public String create(@Validated TaskForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return showCreationForm(form);
+        }
         taskService.create(form.toEntity());
         return "redirect:/tasks";
     }
--- src/main/java/com/example/todo/controller/task/TaskForm.java
+++ src/main/java/com/example/todo/controller/task/TaskForm.java
@@ -2,10 +2,13 @@ package com.example.todo.controller.task;
 
 import com.example.todo.service.task.TaskEntity;
 import com.example.todo.service.task.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
 
 public record TaskForm(
+        @NotBlank
         String summary,
         String description,
+        @NotBlank
         String status
 ) {
     public TaskEntity toEntity() {
